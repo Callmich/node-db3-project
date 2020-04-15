2e96fc3dcbe16dafdd83c4e6d2ee03b919c37c73
Michael Callahan <CallahanMichaelE@gmail.com>
2020-04-15 16:02:57 -0600

    added a db config file as well as added the first helper function find()

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,10 +1,17 @@
-const Schemes = require('./scheme-model.js');
+const dB = require('../data/db-config.js')
 
 module.exports = {
     find,
-    findById,
-    findSteps,
-    add,
-    update,
-    remove
+    // findById,
+    // findSteps,
+    // add,
+    // update,
+    // remove
+}
+
+//Calling find returns a promise that resolves to an array of all schemes in the database.
+//No steps are included.
+
+function find(){
+    return dB('schemes');
 }
