e7cd1bd63f0512230539853652668032d56e7669
Michael Callahan <CallahanMichaelE@gmail.com>
2020-04-15 17:07:45 -0600

    add function has been created

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -4,7 +4,7 @@ module.exports = {
     find,
     findById,
     findSteps,
-    // add,
+    add,
     // update,
     // remove
 }
@@ -30,5 +30,21 @@ function findSteps(scheme_id){
     .join("schemes as sc", "st.scheme_id", "sc.id")
     .select("st.id", "sc.scheme_name", "st.step_number", "st.instructions")
     .orderBy("st.step_number")
+}
 
+// add(scheme):
+// Expects a scheme object.
+// Inserts scheme into the database.
+// Resolves to the newly inserted scheme, including id.
+
+function add(scheme){
+    return dB('schemes')
+    .insert(scheme, "id")
+    .then(([id])=>{
+        console.log(id)
+        return findById(id)
+    })
+    .catch(error=>{
+        console.log("Here is the error", error)
+    })
 }
